776027eed62f469c784d709c776132ee2c084e05
oceanByte <albert.peci@posteo.de>
2021-03-05 13:53:11 +0100

    refactor (home): add line breaks

--- src/frontend/src/pages/Home/Home.view.tsx
+++ src/frontend/src/pages/Home/Home.view.tsx
@@ -13,7 +13,7 @@ export const HomeView = () => {
 
           <section className={"components first"}>
             <div className={"left"}>
-              <h1>Build a Web3 App the fun way!</h1>
+              <h1>Build a Web3 App <br /> the fun way!</h1>
               <p>NEAR Academy is an interactive introduction course for web developers looking to explore blockchain integration to their Apps. It is free, and it hands a completion certificate.</p>
               <Link to="/near101/chapter-1">
                 <Button text="START ACADEMY" color="primary" />
@@ -24,7 +24,7 @@ export const HomeView = () => {
           <section className={"components second"}>
             <h1>Join the future</h1>
             <div className={"left"}>
-              <img src={'man.svg'}/>
+              <img src={'man.svg'} />
               <p>The decentralized internet, or Web3, has been growing at a fast pace since its inception in 2008. It is now a magnet to talent, with more smart developers joining every day. Web3 monthly developers are up +15% over 2020, following investments in venture capital of about 900 Mio USD. </p>
               <p>This is not without similarities to the growth of internet services in the 2000s, and hints at significant development for years to come. Importantly, the infrastructure has matured, and makes it easy to create dApps, the decentralized equivalent to Apps. Do you have NPM installed?</p>
             </div>
@@ -37,7 +37,7 @@ export const HomeView = () => {
 
           <section className={"components third"}>
             <div className={"left"}>
-              <h1>Immerse yourself in the course narrative</h1>
+              <h1>Immerse yourself  <br />  in the course narrative</h1>
               <p>You are about to play a web developer at the Meme Museum.</p>
               <p>The year is 2029, the near future; things are not that different from today, except perhaps that life is 80% digital now. Innovation that seemed breakthrough in the early 20s is widely adopted, and so is NEAR protocol in the blockchain space.</p>
               <p>The new NFT Meme museum in Palo Alto is the hottest thing right now. Elon Musk was there at its inauguration last week, that says something.</p>
@@ -45,18 +45,18 @@ export const HomeView = () => {
                 <Button text="GET STARTED" color="primary" />
               </Link>
             </div>
-            <img className={"museum-interior"} src={'museum_interior_x2.svg'}/>
+            <img className={"museum-interior"} src={'museum_interior_x2.svg'} />
           </section>
 
           <footer className={"footer"}>
             <div className={"left"}>
-              <img src={'logo-white.svg'}/>
+              <img src={'logo-white.svg'} />
               <p>Made by 101Labs.org</p>
             </div>
             <div className={"right"}>
-              <a href={"https://github.com"}> <img src={'github.svg'}/></a>
-              <a href={"https://twitter.com"}><img src={'twitter.svg'}/></a>
-              <a href={"https://telegram.com"}><img src={'telegram.svg'}/></a>
+              <a href={"https://github.com"}> <img src={'github.svg'} /></a>
+              <a href={"https://twitter.com"}><img src={'twitter.svg'} /></a>
+              <a href={"https://telegram.com"}><img src={'telegram.svg'} /></a>
             </div>
           </footer>
 
