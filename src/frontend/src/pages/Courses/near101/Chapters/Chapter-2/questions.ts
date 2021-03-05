350d82a84b4593bd45c44c518c35c6020ddbdbf3
oceanByte <albert.peci@posteo.de>
2021-03-05 13:51:57 +0100

    content (chapters): add exercises

---
+++ src/frontend/src/pages/Courses/near101/Chapters/Chapter-2/questions.ts
@@ -0,0 +1,12 @@
+export const questions = [
+    {
+        question: "Select all available jobs from decentrajob.testnet",
+        answers: [
+            "A",
+            "B",
+            "C",
+            "D"
+        ],
+        responses: ["A"],
+    }
+]
