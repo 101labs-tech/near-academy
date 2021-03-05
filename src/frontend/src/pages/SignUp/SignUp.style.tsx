4c5389aff2bd012d226d79179ca241d55d4aa424
oceanByte <albert.peci@posteo.de>
2021-03-05 15:54:16 +0100

    fix (sign up): center align sign up button and fix bg color

--- src/frontend/src/pages/SignUp/SignUp.style.tsx
+++ src/frontend/src/pages/SignUp/SignUp.style.tsx
@@ -1,22 +1,29 @@
 import styled from 'styled-components/macro'
 
-import { Card, CardPage, FadeInFromTop, primaryColor } from '../../styles'
+import { Card, CardPage, FadeInFromTop } from '../../styles'
 
-export const SignUpStyled = styled(CardPage)``
+export const SignUpStyled = styled(CardPage)`
+  height: 90vh;
+`
 
 export const SignUpCard = styled(Card)`
+  text-align: center;
   padding: 20px;
 `
 export const SignUpSeparator = styled.div`
   height: 10px;
 `
 
-export const SignUpTitle = styled(FadeInFromTop)``
+export const SignUpTitle = styled(FadeInFromTop)`
+  text-align: center;
+  color: white;
+`
 
 export const SignUpLogin = styled.div`
   margin-top: 10px;
   text-align: center;
+  width: 100%;
   > a {
-    color: ${primaryColor} !important;
+    color: white !important;
   }
 `
