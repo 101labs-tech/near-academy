3d7eac3d7c0d6fc08c655746081c0b2e65616470
Serhii Hurshal <seergiygurshall@gmail.com>
2021-09-08 12:52:13 +0300

    feture(footer mobile): add previous button

--- src/frontend/src/pages/Chapter/Footer/Footer.style.tsx
+++ src/frontend/src/pages/Chapter/Footer/Footer.style.tsx
@@ -2,13 +2,14 @@ import styled from 'styled-components/macro'
 
 export const FooterStyled = styled.div`
   height: 70px;
-  background: #F2F2F2;
+  background: #f2f2f2;
   margin-top: 20px;
   display: flex;
   align-items: center;
 
   @media (max-width: 900px) {
-    margin: 0;
+    margin: 0 10px;
+    justify-content: space-between;
   }
 
   a {
@@ -20,18 +21,17 @@ export const FooterStyled = styled.div`
   }
 
   @media (max-width: 900px) {
-    > a:nth-child(1) {
-      display: none;
-    }
     button {
       margin: 5px 0;
+      height: 50px;
+      width: 150px !important;
     }
   }
 `
 export const ProgressBarWrapper = styled.div`
-    width: 100%;
-    
-     @media (max-width: 900px) {
-        display:none;
-     }
+  width: 100%;
+
+  @media (max-width: 900px) {
+    display: none;
+  }
 `
