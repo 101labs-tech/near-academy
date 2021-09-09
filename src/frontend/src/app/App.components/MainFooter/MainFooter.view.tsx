7407ff0ec16f8bb29b3b005521e2410d2e83a4d6
Serhii Hurshal <seergiygurshall@gmail.com>
2021-09-09 11:14:16 +0300

    feature(header, mainfooter): remove refferal link from footer and add it to navbar

--- src/frontend/src/app/App.components/MainFooter/MainFooter.view.tsx
+++ src/frontend/src/app/App.components/MainFooter/MainFooter.view.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
-import {Footer} from './MainFooter.style'
-import {Link} from "react-router-dom";
-import {PublicUser} from "../../../shared/user/PublicUser";
-import * as PropTypes from "prop-types";
-import {HeaderView} from "../Header/Header.view";
-
+import React from 'react'
+import { Footer } from './MainFooter.style'
+import { Link } from 'react-router-dom'
+import { PublicUser } from '../../../shared/user/PublicUser'
+import * as PropTypes from 'prop-types'
 
 type FooterViewProps = {
-    user?: PublicUser
+  user?: PublicUser
 }
 
-export const ViewMainFooter  = ({user}: FooterViewProps) => {
-    return (
-        <Footer>
-            <div className="footer-wrapper">
-                <div className={'left'}>
-                    <img src={'logo-white.svg'} alt="101 labs icon" />
-                    { user ? <Link className={'link'} to="/invite">REFERRAL SYSTEM</Link> : null}
-                    <Link className={'link'} to="/terms">TERMS</Link>
-                    <p>Made by <a target='_blank' href="https://101Labs.org">101Labs.org</a></p>
-                </div>
-                <div className={'right'}>
-                    {/* <div className="links">
+export const ViewMainFooter = ({ user }: FooterViewProps) => {
+  return (
+    <Footer>
+      <div className="footer-wrapper">
+        <div className={'left'}>
+          <img src={'logo-white.svg'} alt="101 labs icon" />
+          <Link className={'link'} to="/terms">
+            TERMS
+          </Link>
+          <p>
+            Made by{' '}
+            <a target="_blank" href="https://101Labs.org">
+              101Labs.org
+            </a>
+          </p>
+        </div>
+        <div className={'right'}>
+          {/* <div className="links">
               <a href="https://mintbase.io/" rel="noopener noreferrer" target="_blank" className="link">Mintbase</a>
               <a href="https://paras.id/" rel="noopener noreferrer" target="_blank" className="link">Paras</a>
               <a href="https://berryclub.io/" rel="noopener noreferrer" target="_blank" className="link">Berry Club</a>
@@ -29,26 +33,26 @@ export const ViewMainFooter  = ({user}: FooterViewProps) => {
               <a href="https://nearguilds.com/" rel="noopener noreferrer" target="_blank" className="link">Near Guilds</a>
               <a href="https://near.chat" rel="noopener noreferrer" target="_blank" className="link">Near Chat</a>
             </div> */}
-                    <div className="socials">
-                        <a href={'https://github.com/near'}>
-                            {' '}
-                            <img src={'github.svg'} alt="github icon" />
-                        </a>
-                        <a href={'https://twitter.com/NEARProtocol'}>
-                            <img src={'twitter.svg'} alt="twitter icon" />
-                        </a>
-                        <a href={'https://t.me/cryptonear'}>
-                            <img src={'telegram.svg'} alt="telegram icon" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </Footer>
-    )
+          <div className="socials">
+            <a href={'https://github.com/near'}>
+              {' '}
+              <img src={'github.svg'} alt="github icon" />
+            </a>
+            <a href={'https://twitter.com/NEARProtocol'}>
+              <img src={'twitter.svg'} alt="twitter icon" />
+            </a>
+            <a href={'https://t.me/cryptonear'}>
+              <img src={'telegram.svg'} alt="telegram icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  )
 }
 
 ViewMainFooter.propTypes = {
-    user: PropTypes.object,
+  user: PropTypes.object,
 }
 
 ViewMainFooter.defaultProps = {}
